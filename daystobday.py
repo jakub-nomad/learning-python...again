14162a491883e5b128a0e94bc975d7025b1fb399
Jakub Benedyk <443463@edu.cwa.ac.uk>
2023-11-24 12:07:41 +0000

    age calucator and semi-completed days till birthday

---
+++ daystobday.py
@@ -0,0 +1,6 @@
+total = 0
+yearDays = [total := total + i for i in [0,31,28,31,30,31,30,31,31,30,31,30,31]]
+
+DofB = input("Enter date of birth (DD/MM/YYYY): ")
+
+#day, month, _ 
--- main.py
+++ main.py
@@ -1,1 +1,51 @@
-print("I am not happy")
+from datetime import *
+today = date.today()
+birthdayHappened = False
+minYear = 1900
+maxYear = today.year-1
+minMonth = 1
+maxMonth = 12
+minDate = 1
+maxDate = 31
+userYearBirth = 0
+userMonthBirth = 0
+userDayBirth = 0
+
+def setBirthDate():
+    global userYearBirth
+    global userMonthBirth
+    global userDayBirth
+    try:
+        loop = True
+        while loop == True:
+            userYearBirth = int(input("Enter your year of birth: "))
+            while userYearBirth > maxYear or userYearBirth < minYear:
+                userYearBirth = int(input("Please enter a valid year of birth: "))
+
+            userMonthBirth = int(input("Enter your month of birth as a number: "))
+            while userMonthBirth > maxMonth or userMonthBirth < minMonth:
+                userMonthBirth = int(input("Please enter a valid month of birth as a number: "))
+
+            userDayBirth = int(input("Enter your day of birth: "))
+            while userDayBirth > maxDate or userDayBirth < minDate:
+                userDayBirth = int(input("Please enter a valid day of birth: "))
+            loop = False
+    except ValueError:
+            setBirthDate()
+
+setBirthDate()
+
+if userMonthBirth < today.month:
+    birthdayHappened = True
+elif userMonthBirth == today.month:
+    if userDayBirth >= today.day:
+        birthdayHappened = True
+    else:
+        birthdayHappened = False
+else:
+    birthdayHappened = False
+
+if birthdayHappened == True:
+    print(f"You are {today.year-userYearBirth} years old!")
+else:
+    print(f"You are {today.year-userYearBirth-1} years old!")
